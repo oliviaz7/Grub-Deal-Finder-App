608d8498342715bd16763125b852b32ba99decfe
Joyce <joyce.j8123@gmail.com>
2025-02-22 14:31:24 -0500

    add google maps api and filter on longlat radius

---
+++ flask_server/google_maps.py
@@ -0,0 +1,25 @@
+import requests
+import os
+
+MAPS_API_KEY = os.getenv("MAPS_API_KEY")
+NEARBY_SEARCH_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+
+def get_place_ids(latitude, longitude, radius):
+    # Prepare the parameters for the API request
+    params = {
+        "location": f"{latitude},{longitude}",
+        "radius": radius,
+        "key": MAPS_API_KEY,
+        "type": "restaurant",
+    }
+
+    # Make the request to the Google Places API
+    response = requests.get(NEARBY_SEARCH_URL, params=params)
+
+    if response.status_code != 200:
+        return None
+
+    places = response.json()
+    place_ids = [place["place_id"] for place in places.get("results", [])]
+
+    return place_ids
--- flask_server/server.py
+++ flask_server/server.py
@@ -4,6 +4,7 @@ import os
 from datetime import datetime
 from dotenv import load_dotenv
 from supabase import create_client, Client
+from google_maps import get_place_ids
 
 load_dotenv()
 url = os.getenv("SUPABASE_URL")
@@ -18,6 +19,12 @@ CORS(app)
 def iso_to_unix(iso_date):
 	return int(datetime.fromisoformat(iso_date.replace("Z", "+00:00")).timestamp() * 1000)
 
+@app.route('/all_restaurant_deals', methods=["GET"])
+def get_all_deals():
+	result = supabase.from_('Restaurant').select('*, Deal(*)').execute()
+	result_data = result.data
+	return jsonify(result_data)
+
 # TODO: connect to Google Maps API to get a list of place ids given coordinates and radius. Currently gets all restaurant deals.
 @app.route('/restaurant_deals', methods=["GET"])
 def get_deals():
@@ -25,9 +32,17 @@ def get_deals():
 	longitude = request.args.get('longitude')
 	radius = request.args.get('radius')
 
-	result = supabase.from_('Restaurant').select('*, Deal(*)').execute()
+	# google maps api call to get place id
+	place_ids = get_place_ids(latitude, longitude, radius)
+
+	if not place_ids:
+		return jsonify({"error": "Failed to retrieve places from Google Maps API"}), 500
+
+	# Supabase query to get fatty restaurant deals
+	result = supabase.from_('Restaurant').select('*, Deal(*)').in_('place_id', place_ids).execute()
 	result_data = result.data
 
+	# clean up data to send to android app
 	for restaurant in result_data:
 		restaurant['coordinates'] = {
 			"latitude": restaurant['latitude'],
