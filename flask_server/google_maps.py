b004a07bd83fe35e0b4c72918da89fbec636777c
Joyce <joyce.j8123@gmail.com>
2025-03-05 18:19:56 -0500

    added logging and better error handling

--- flask_server/google_maps.py
+++ flask_server/google_maps.py
@@ -1,25 +1,41 @@
 import requests
 import os
+import logging
+
+logger = logging.getLogger('werkzeug')
 
 MAPS_API_KEY = os.getenv("MAPS_API_KEY")
 NEARBY_SEARCH_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
 WEBSITE_URL = "https://maps.googleapis.com/maps/api/place/details/json"
 
+def _check_api_key():
+    if not MAPS_API_KEY:
+        logger.error("Missing API Key: MAPS_API_KEY is not set.")
+        return False
+    return True
 def get_restaurant_website(place_id):
+    if not _check_api_key():
+        return None
+
     params = {
         "place_id": place_id,
         "fields": "name,website",
         "key": MAPS_API_KEY
     }
     response = requests.get(WEBSITE_URL, params=params)
-    data = response.json()
 
     if response.status_code != 200:
+        logger.error(f"Failed API Request: {WEBSITE_URL} responded with status code {response.status_code}.")
         return None
+
+    data = response.json()
 
     return data.get("result", {}).get("website", None)
 
 def search_nearby_restaurants(keyword, latitude, longitude, radius):
+    if not _check_api_key():
+        return []
+
     params = {
         "keyword": keyword,
         "location": f"{latitude},{longitude}",
@@ -30,6 +46,10 @@ def search_nearby_restaurants(keyword, latitude, longitude, radius):
 
     # Make the request to the Google Places API
     response = requests.get(NEARBY_SEARCH_URL, params=params)
+
+    if response.status_code != 200:
+        logger.error(f"API Request Error for search_nearby_restaurants")
+        return []
 
     places = response.json()
 
@@ -48,23 +68,4 @@ def search_nearby_restaurants(keyword, latitude, longitude, radius):
 
     return nearby_restaurants
 
-# TODO: NOT USED ANYMORE
-def get_place_ids(latitude, longitude, radius):
-    # Prepare the parameters for the API request
-    params = {
-        "location": f"{latitude},{longitude}",
-        "radius": radius,
-        "key": MAPS_API_KEY,
-        "type": "restaurant",
-    }
 
-    # Make the request to the Google Places API
-    response = requests.get(NEARBY_SEARCH_URL, params=params)
-
-    if response.status_code != 200:
-        return None
-
-    places = response.json()
-    place_ids = [place["place_id"] for place in places.get("results", [])]
-
-    return place_ids
