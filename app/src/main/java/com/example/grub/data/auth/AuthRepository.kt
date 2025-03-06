12d40e7a71d7db5574b12f6765b6e86106c30f65
oliviaz7 <62443128+oliviaz7@users.noreply.github.com>
2025-03-05 20:47:19 -0500

    refactored user and logged in global state

--- app/src/main/java/com/example/grub/data/auth/AuthRepository.kt
+++ app/src/main/java/com/example/grub/data/auth/AuthRepository.kt
@@ -1,12 +1,8 @@
 package com.example.grub.data.auth
 import com.example.grub.data.Result
+import com.example.grub.model.User
 import kotlinx.coroutines.flow.StateFlow
 
-data class UserProfile(
-    val id: String,
-    val username: String,
-    val email: String,
-)
 /**
  * Manages the data layer related to authentication and user profile information.
  * Is the single source of truth for authentication data, functions, and user profile details
@@ -14,6 +10,5 @@ data class UserProfile(
 interface AuthRepository {
     suspend fun login(username: String, password: String): Result<String> // Returns token
     suspend fun logout()
-    val isLoggedIn: StateFlow<Boolean>
-    suspend fun getUserProfile(): Result<UserProfile>
+    val loggedInUser: StateFlow<User?>
 }
--- app/src/main/java/com/example/grub/data/auth/impl/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/grub/data/auth/impl/AuthRepositoryImpl.kt
@@ -1,26 +1,22 @@
 package com.example.grub.data.auth.impl
 
-import com.example.grub.data.auth.AuthRepository
-import com.example.grub.data.auth.UserProfile
 import com.example.grub.data.Result
+import com.example.grub.data.auth.AuthRepository
+import com.example.grub.model.User
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class AuthRepositoryImpl : AuthRepository {
     // TODO: look into using android shared preferences to persist any tokens
-    private val _isLoggedIn = MutableStateFlow(false)
-    override val isLoggedIn: StateFlow<Boolean> = _isLoggedIn.asStateFlow()
+    private val _loggedInUser = MutableStateFlow<User?>(null)
+    override val loggedInUser: StateFlow<User?> = _loggedInUser.asStateFlow()
 
     override suspend fun login(username: String, password: String): Result<String> {
         TODO("Not yet implemented")
     }
 
     override suspend fun logout() {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getUserProfile(): Result<UserProfile> {
         TODO("Not yet implemented")
     }
 }
